0929e8046ee559e0ee18c80ce37d010831b6ef3e
Omodot <omodote@hotmail.com>
2018-11-09 13:51:21 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CarRentaljava
+CarRentalJava
---
+++ src/rentacar/RentAcar.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rentacar;
+
+/**
+ *
+ * @author omodo_000
+ */
+public class RentAcar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login1 u=new Login1();
+        u.setVisible(true);
+        u.setSize(665, 450);
+        
+       
+        
+    }
+    
+}
